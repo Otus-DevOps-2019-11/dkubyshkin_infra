3f2e1f4e05d23c5fb8260c65ca2f9bb41bed8428
dkubyshkin <dlyrabotyyy>
2020-01-16 22:01:52 +0300

    HomeWork Terraform-1

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key {
+  description = "private_key"
+}
+variable zone {
+  description = "zone"
+  default     = "europe-west1-b"
+}
