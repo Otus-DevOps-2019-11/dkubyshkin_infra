dbcf5c0ad3d1cc7b996e29d5df6725eaf732c7cd
dkubyshkin <dlyrabotyyy>
2020-01-25 21:54:55 +0300

    fix storage-bucket.tf

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,13 +1,13 @@
 provider "google" {
   version = "~> 2.15"
-  project = "infra-262510"
-  region  = "europe-west1"
+  project = var.project
+  region  = var.region
 }
 
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.3.0"
-  location = "europe-west1-b"
+  location = "europe-west1"
 
   # Имя поменяйте на другое
   name = "storage-bucket-appreddit"
